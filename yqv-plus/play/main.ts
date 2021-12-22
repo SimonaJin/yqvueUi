27bd45e098dc5ccd0490a9847b62ff6053fa6575
Simona <136150620@qq.com>
2021-12-22 14:58:26 +0800

    yqc-plus 修复问题-node_modules
    yqc-plus 修复问题-git

--- yqv-plus/play/main.ts
+++ yqv-plus/play/main.ts
@@ -1,8 +1,8 @@
 import {createApp} from 'vue'
 import App from './app.vue';
-import YqIcon from 'yqv-plus'
-import 'yqv-plus/dist/index.css';
+// import YqIcon from 'yqv-plus'
+// import 'yqv-plus/dist/index.css';
 const app = createApp(App);
 
-app.use(YqIcon);
+// app.use(YqIcon);
 app.mount('#app')
